1db3c6d66c0c9b61059c1b28e1c1e784b2bc989a
RehamanRamz <rehaman.ramzaula@gmail.com>
2023-03-29 13:01:31 +0300

    updated serve.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const notes = require("./data/notes");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+
 app.get("/", function (req, res) {
   res.send("API is running");
 });
+
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
+
 app.get("/api/notes/:id", (req, res) => {
   const note = notes.find((n) => n._id === req.params.id);
   res.send(note);
